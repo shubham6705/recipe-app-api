fc8f57196740ccdaf1b98b27c0031d4c24f9917f
shubham6705 <shubhamsharma6705@gmail.com>
2022-10-16 23:47:08 +0530

    some changes in settings for workflow Actions two.

--- app/app/settings.py
+++ app/app/settings.py
@@ -17,13 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See 
-# https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o^i' \
-                '=tmwkum#59@q4kg=qug@shz&62b6f&!' \
-                '*%dw+*x%lh0c!e3h'
+SECRET_KEY = 'django-insecure-o^i=tmwkum#59@q4kg=qug@shz&62b6f&!*%dw+*x%lh0c!e3h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,26 +82,21 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/
-# settings/#auth-password-validators
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-{
-    'NAME': 
-'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-},
-{
-    'NAME': 
-'django.contrib.auth.password_validation.MinimumLengthValidator',
-},
-{
-    'NAME': 
-'django.contrib.auth.password_validation.CommonPasswordValidator',
-},
-{
-    'NAME': 
-'django.contrib.auth.password_validation.NumericPasswordValidator',
-},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
